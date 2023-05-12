35aa4eafb54d1057f77b9673371a468f0a6ecd0d
Mibu287 <buiducminh287@gmail.com>
2023-05-12 18:18:31 +0700

    Handle multiple relay servers

--- src/main.rs
+++ src/main.rs
@@ -8,17 +8,43 @@ mod utils;
 
 use crate::config::Config;
 
-#[tokio::main]
-async fn main() -> anyhow::Result<()> {
-    utils::setup_logger();
-
-    let config = Config::from_file("config.toml").await?;
-
-    let server_addr = SocketAddr::from_str(&config.target_server)?;
+async fn serve(config: Config) {
+    let server_addr = match SocketAddr::from_str(&config.target_server) {
+        Ok(addr) => addr,
+        Err(err) => {
+            log::error!(
+                "Failed to resolve socket address {} with error {}",
+                &config.relay_server,
+                err.to_string()
+            );
+            return;
+        }
+    };
     log::info!("Target server is at address {}", server_addr);
 
-    let local_addr = SocketAddr::from_str(&config.relay_server)?;
-    let listener = TcpListener::bind(local_addr).await?;
+    let local_addr = match SocketAddr::from_str(&config.relay_server) {
+        Ok(addr) => addr,
+        Err(err) => {
+            log::error!(
+                "Failed to resolve socket address {} with error {}",
+                &config.relay_server,
+                err.to_string()
+            );
+            return;
+        }
+    };
+
+    let listener = match TcpListener::bind(local_addr).await {
+        Ok(listener) => listener,
+        Err(err) => {
+            log::error!(
+                "Failed to listen at address {} with error {}",
+                local_addr,
+                err.to_string()
+            );
+            return;
+        }
+    };
     log::info!("TCP relay server is listening at address {}", local_addr);
 
     loop {
@@ -35,4 +61,29 @@ async fn main() -> anyhow::Result<()> {
             }
         };
     }
+}
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    utils::setup_logger();
+
+    let configs = Config::from_file("config.json").await?;
+    let handles: Vec<_> = configs
+        .into_iter()
+        .map(move |config| tokio::spawn(serve(config)))
+        .collect();
+
+    for handle in handles {
+        match handle.await {
+            Ok(_) => {}
+            Err(err) => {
+                log::error!(
+                    "Error occured while joining relay server {}",
+                    err.to_string()
+                );
+            }
+        }
+    }
+
+    Ok(())
 }
