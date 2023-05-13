27c3c984c2ab525fc65c7a1868c94ba5969fb0f4
Mibu287 <buiducminh287@gmail.com>
2023-05-13 10:17:24 +0700

    Implement message passing to manage connection count

--- src/book_keeper.rs
+++ src/book_keeper.rs
@@ -5,7 +5,10 @@ use std::{
     sync::Arc,
 };
 
-use tokio::sync::Mutex;
+use tokio::sync::{
+    mpsc::{UnboundedReceiver, UnboundedSender},
+    Mutex,
+};
 
 #[derive(Copy, Clone, Debug, Eq, PartialEq, Hash)]
 pub(crate) struct ConnectionKey {
@@ -14,26 +17,53 @@ pub(crate) struct ConnectionKey {
     pub target_server: SocketAddr,
 }
 
-#[derive(Clone, Debug, Default)]
-pub(crate) struct ConnectionRegistry(Arc<Mutex<HashMap<ConnectionKey, usize>>>);
+pub(crate) enum ConnectionEvent {
+    New(ConnectionKey),
+    Disconnect(ConnectionKey),
+}
 
-impl ConnectionRegistry {
-    pub(crate) async fn increase(&self, connection: ConnectionKey) {
-        let mut guard = self.0.lock().await;
-        let count = guard.deref_mut().entry(connection).or_insert(0);
+#[derive(Debug)]
+pub(crate) struct ConnBookKeeper {
+    receiver: UnboundedReceiver<ConnectionEvent>,
+    counter: Arc<Mutex<HashMap<ConnectionKey, usize>>>,
+}
+
+impl ConnBookKeeper {
+    pub(crate) fn new() -> (ConnBookKeeper, UnboundedSender<ConnectionEvent>) {
+        let (sender, receiver) = tokio::sync::mpsc::unbounded_channel();
+        let counter = Arc::new(Mutex::new(HashMap::new()));
+        (Self { receiver, counter }, sender)
+    }
+
+    pub(crate) async fn process_events(mut self) {
+        while let Some(event) = self.receiver.recv().await {
+            match event {
+                ConnectionEvent::New(conn) => self.increase(conn).await,
+                ConnectionEvent::Disconnect(conn) => self.decrease(conn).await,
+            }
+        }
+    }
+
+    async fn increase(&self, connection: ConnectionKey) {
+        let mut guard = self.counter.lock().await;
+        let count = guard.deref_mut().entry(connection).or_default();
         *count += 1;
     }
 
-    pub(crate) async fn decrease(&self, connection: ConnectionKey) {
-        let mut guard = self.0.lock().await;
-        guard
-            .deref_mut()
+    async fn decrease(&self, connection: ConnectionKey) {
+        let mut guard = self.counter.lock().await;
+        let count = guard
             .entry(connection)
-            .and_modify(|count| *count -= 1);
+            .and_modify(|count| *count -= 1)
+            .or_default();
+
+        if *count == 0 {
+            guard.remove_entry(&connection);
+        }
     }
 
     pub(crate) async fn copy_inner(&self) -> HashMap<ConnectionKey, usize> {
-        let guard = self.0.lock().await;
+        let guard = self.counter.lock().await;
         guard.deref().clone()
     }
 }
